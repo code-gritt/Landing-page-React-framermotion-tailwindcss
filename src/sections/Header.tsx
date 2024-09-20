2c90dad5da2be82d0a125ded71fe63132263714c
gokul-saaragh <gokulchandan24@gmail.com>
2024-09-20 14:08:14 +0530

    created header comp

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export const Navbar = () => {
   return (
     <>
-      <header className="sticky top-0 backdrop-blur-sm">
+      <header className="sticky top-0 backdrop-blur-sm z-20">
         <div className="flex justify-center items-center py-3 gap-3 bg-black text-white text-sm">
           <p className="text-white/60 hidden md:block">
             Streamline your workflow and boost your productivity
